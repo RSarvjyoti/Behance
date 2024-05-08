31abcb1f75779f0f30140804cb6b5983ff5fb45c
unknown <shubhamkhangar45@gmail.com>
2024-05-09 00:15:21 +0530

    day 2 push

---
+++ Frontend/my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+
+
+export const Navbar=()=>{
+    return(
+        <>
+        <div className="navbar">
+            {/*login logo  */}
+            <div>
+              <h2>login signup</h2>
+            </div>
+            {/* haan logo */}
+            <div>
+                <h2>Haan Logo</h2>
+                <img src="Hann_logo.png" />
+            </div>
+            {/* search icon and add to card */}
+            <div>
+            <h2>search and add to card</h2>
+            </div>
+        </div>
+        </>
+    )
+}
