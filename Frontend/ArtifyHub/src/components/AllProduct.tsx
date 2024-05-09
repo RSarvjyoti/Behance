2cff7f90f3fa80b8c822790ed0458e431e34787d
RSarvjyoti <sarvjyoti64@gmail.com>
2024-05-09 18:12:57 +0530

    Style the card

--- Frontend/ArtifyHub/src/components/AllProduct.tsx
+++ Frontend/ArtifyHub/src/components/AllProduct.tsx
@@ -1,19 +1,23 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Container, Grid, GridItem, Box, Flex, Button } from '@chakra-ui/react'
+import { Container, Grid, GridItem, Box, Flex, Button, Card, CardHeader, CardBody, CardFooter, 
+    Image,Stack, Heading, Text, Divider
+ } from '@chakra-ui/react'
 
 export default function AllProduct() {
     const [myData, setData] = useState([]);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         axios.get('https://behance-z9se.onrender.com/data')
             .then((res) => {
                 setData(res.data);
             })
+            .catch((error) => setError(error.massage))
     }, [])
 
     return (
-        
+
         <Container maxW={['container.sm', 'container.md', 'container.lg', 'container.xl']} centerContent>
             <Flex direction='row' justifyContent='space-between' p={2} w={'100%'}>
                 <Button colorScheme='black' variant='outline'>
@@ -28,14 +32,37 @@ export default function AllProduct() {
                     <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)', 'repeat(4, 1fr)']} gap={3}>
                         {myData.map((post) => {
 
-                            const { id, img_src, img_src_2, title, price_item, price_item_2, price_item_3  } = post;
+                            const { id, img_src, img_src_2, title, price_item, price_item_2, price_item_3 } = post;
 
                             return (
                                 <GridItem key={id} w='100%' h='auto'>
-                                    <div className='card'>
+                                    {/* <div className='card'>
                                         <img src={img_src} alt="" />
                                         <p>{title}</p>
-                                    </div>
+                                    </div> */}
+
+                                    <Card maxW='sm'>
+                                        <CardBody>
+                                            <Image
+                                                src={img_src}
+                                                alt='Green double couch with wooden legs'
+                                            />
+                                            <Stack mt='6' spacing='3'>
+                                                <Text text-aling >
+                                                    <a href="#">{title}</a>
+                                                </Text>
+                                                <Text color='blue.600' fontSize='2xl'>
+                                                 {price_item_2}
+                                                </Text>
+                                            </Stack>
+                                        </CardBody>
+                                        <CardFooter  style={{ display: 'flex', justifyContent: 'center' }}>
+                                                <Button borderRadius= '50px' border={'1px'} bg='none' >
+                                                    Add to cart
+                                                </Button>
+                                        </CardFooter>
+                                    </Card>
+
                                 </GridItem>
                             )
                         })}
