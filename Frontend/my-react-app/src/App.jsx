1afb1773c987cc029b40f699fefafefee9ac7884
unknown <shubhamkhangar45@gmail.com>
2024-05-09 22:22:06 +0530

    day3 work push

--- Frontend/my-react-app/src/App.jsx
+++ Frontend/my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
 
 function App() {
 
-
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
