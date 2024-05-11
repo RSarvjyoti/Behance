74e9a8cf1233621a4d167b7199b0ecb016e02cfb
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-05-11 22:36:01 +0530

    Staging changes

--- Frontend/ArtifyHub/product_desc/src/App.js
+++ Frontend/ArtifyHub/product_desc/src/App.js
@@ -1,27 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
-import ProductDescriptionPage from "./component/ProductDescriptionPage";
+import ProductDescriptionPage from "./components/ProductDescriptionPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
       <ChakraProvider>
-        <ProductDescriptionPage productId={5} />
+        <ProductDescriptionPage productId={8} />
       </ChakraProvider>
     </div>
   );
