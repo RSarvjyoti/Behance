34d2905adc5684dc37a25ce0006a8ff45e8a27aa
unknown <shubhamkhangar45@gmail.com>
2024-05-12 23:50:02 +0530

    updated data

--- Frontend/my_app/src/components/Main.jsx
+++ Frontend/my_app/src/components/Main.jsx
@@ -18,16 +18,16 @@ export const MainContent=()=>{
           <h1>Image 2</h1>
           <br />
           <h1>Keep Your Skin Healthy On The Move!</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-1'} />
+          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-2'} />
 
           <br />
           <h1>Shop Our Best Sellers</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-1'} />
+          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-3'} />
 
           <h1>Image 3</h1>
           <br />
           <h1>Our Refills</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-1'} />
+          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-4'} />
 
           <h1>Image 4</h1>
         </div>
