238281f840b6020fa3d0bb4b16b9d5b606cda097
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-05-13 06:32:00 +0530

    Staging changes

--- Frontend/my_app/src/App.jsx
+++ Frontend/my_app/src/App.jsx
@@ -1,18 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react";
-// import AllProduct from "./components/AllProduct";
 import { Home } from "./components/Home";
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MyCarousel } from "./components/Carousel";
+import ProductDescriptionPage from "./components/ProductDescriptionPage";
 
 function App() {
-
   return (
-    <>
-      <ChakraProvider>
-      <Home />
-        {/* <AllProduct /> */}
-      </ChakraProvider>
-    </>
-  )
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/carousel"
+            element={<MyCarousel fetchUrl="your-fetch-url" />}
+          />
+          <Route
+            path="/product/:productId"
+            element={<ProductDescriptionPage  productId={productId}/>}
+          />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- Frontend/my_app/src/components/Main.jsx
+++ Frontend/my_app/src/components/Main.jsx
@@ -1,37 +1,55 @@
-import {  MyCarousel } from "./Carousel";
-import home1 from '../Images/home-1.jpg'
-import home2 from '../Images/home-2.webp'
-import home3 from '../Images/home-3.webp'
-import home4 from '../Images/home-4.webp'
+import { MyCarousel } from "./Carousel";
+import home1 from "../Images/home-1.jpg";
+import home2 from "../Images/home-2.webp";
+import home3 from "../Images/home-3.webp";
+import home4 from "../Images/home-4.webp";
 import { Image } from "@chakra-ui/react";
+import ProductDescriptionPage from "./ProductDescriptionPage";
 
-export const MainContent=()=>{
-    
-      return (
-        <>
-      
-        <div>
-           <Image src={home1} />
-           <br />
-           <h1 >Shop our Hydrating Hand Sanitizers</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-1'} />
+export const MainContent = () => {
+  const handleCardClick = (productId) => {
+    // Navigate to ProductDescriptionPage with the product ID
+    console.log(
+      `Navigate to ProductDescriptionPage with productId: ${productId}`
+    );
+    <ProductDescriptionPage productId={productId} />;
+  };
+  return (
+    <>
+      <div>
+        <Image src={home1} />
+        <br />
+        <h1>Shop our Hydrating Hand Sanitizers</h1>
+        <MyCarousel
+          fetchUrl={"https://behance-z9se.onrender.com/home-1"}
+          onCardClick={handleCardClick}
+        />
 
-          <Image src={home2} />
-          <br />
-          <h1>Keep Your Skin Healthy On The Move!</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-2'} />
+        <Image src={home2} />
+        <br />
+        <h1>Keep Your Skin Healthy On The Move!</h1>
+        <MyCarousel
+          fetchUrl={"https://behance-z9se.onrender.com/home-2"}
+          onCardClick={handleCardClick}
+        />
 
-          <br />
-          <h1>Shop Our Best Sellers</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-3'} />
+        <br />
+        <h1>Shop Our Best Sellers</h1>
+        <MyCarousel
+          fetchUrl={"https://behance-z9se.onrender.com/home-3"}
+          onCardClick={handleCardClick}
+        />
 
-          <Image src={home3} />
-          <br />
-          <h1>Our Refills</h1>
-          <MyCarousel fetchUrl={'https://behance-z9se.onrender.com/home-4'} />
+        <Image src={home3} />
+        <br />
+        <h1>Our Refills</h1>
+        <MyCarousel
+          fetchUrl={"https://behance-z9se.onrender.com/home-4"}
+          onCardClick={handleCardClick}
+        />
 
-          <Image src={home4} />
-        </div>
-        </>
-      );
-    };
+        <Image src={home4} />
+      </div>
+    </>
+  );
+};
