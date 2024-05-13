8c1416b3e302c7130ceaba4cd049386a41ae53ac
harshalkale0911 <hkale6888@gmail.com>
2024-05-13 17:52:08 +0530

    admin update

--- Frontend/my_app/src/components/Crud.jsx
+++ Frontend/my_app/src/components/Crud.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../Crud.css'; // Assuming this is your CSS file for styling
+import './Crud.css'; // Assuming this is your CSS file for styling
 
 const API_URL = 'https://behance-z9se.onrender.com/data';
 
